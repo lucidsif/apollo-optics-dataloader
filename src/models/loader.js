79ee89ae6ed01ac37621d96c432271aaf7de2066
Dave Hudson <dave@applification.net>
2016-03-15 19:46:54 +0000

    Added more endpoints to loader

--- src/models/loader.js
+++ src/models/loader.js
@@ -7,6 +7,9 @@ module.exports = function () {
     film: Film(),
     character: Character(),
     planet: Planet(),
+    species: Species(),
+    starship: Starship(),
+    vehicle: Vehicle()
   };
 };
 
@@ -37,7 +40,33 @@ function Planet() {
   });
 }
 
-/*
-var film = Film();
-film.loadMany([1, 'http://swapi.co/api/films/3']).then(data => console.log(data));
-*/
+function Species() {
+  return new DataLoader(function (ids) {
+    return axios.all(ids.map(id => {
+      var url = Number.isInteger(id) ? `http://swapi.co/api/species/${id}/` : id;
+      return axios.get(url).then(res => res.data);
+    }));
+  });
+}
+
+function Starship() {
+  return new DataLoader(function (ids) {
+    return axios.all(ids.map(id => {
+      var url = Number.isInteger(id) ? `http://swapi.co/api/starships/${id}/` : id;
+      return axios.get(url).then(res => res.data);
+    }));
+  });
+}
+
+function Vehicle() {
+  return new DataLoader(function (ids) {
+    return axios.all(ids.map(id => {
+      var url = Number.isInteger(id) ? `http://swapi.co/api/vehicles/${id}/` : id;
+      return axios.get(url).then(res => res.data);
+    }));
+  });
+}
+
+
+// var film = Film();
+// film.loadMany([1, 'http://swapi.co/api/films/3']).then(data => console.log(data));
