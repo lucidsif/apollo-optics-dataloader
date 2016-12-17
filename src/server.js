06d6959821624ba8f46ef69b69efb9edc01e136c
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2016-12-17 15:56:08 -0500

    refactored koa-graphql to graphql-server-koa(apollo server)

--- src/server.js
+++ src/server.js
@@ -1,20 +1,37 @@
 const koa = require('koa');
+import koaRouter from 'koa-router';////refactor
+import koaBody from 'koa-bodyparser';////refactor
 const mount = require('koa-mount'); // koa-mount@2.x
 const convert = require('koa-convert');
-const graphqlHTTP = require('koa-graphql');
+//const graphqlHTTP = require('koa-graphql');
+import { graphqlKoa } from 'graphql-server-koa';
 
 const app = new koa();
+const router = new koaRouter();
+const PORT = 3000;
+
+app.use(koaBody());
 
 import { schema } from './index.js';
 var Loader = require('./schema/loader');
 
+/*
 app.use(mount('/graphql', convert(graphqlHTTP({
   schema: schema,
   rootValue: { loader: Loader() },
   graphiql: true
 }))));
+*/
+router.post('/graphql', graphqlKoa((ctx) => {
+  return {
+    schema: myGraphQLSchema,
+    rootValue: { loader: Loader() }
+  };
+ }));
+app.use(router.routes());
+app.use(router.allowedMethods());
 
-const PORT = 3000;
+
 app.listen(PORT);
 
 var status = {
